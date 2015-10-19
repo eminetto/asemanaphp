75f6b9531245823bec2f24a93a9dfee1b16eaefd
Evaldo Junior <evaldojuniorbento@gmail.com>
2015-10-19 23:19:12 +0200

    Corrigi o code style removendo trailing spaces.
    Também adicionei um link para as edicoes anteriores acima do formulário.
    Removi algumas validacoes JavaScript que nao estavam ligadas a um form.

--- js/contact.js
+++ js/contact.js
@@ -1,129 +1,30 @@
-	'use strict';
+'use strict';
 
-    // Signup form
-    $('#signup').validate({
-        rules: {
-            MERGE1: {
-                required: true,
-                minlength: 2
-            },
-            MERGE2: {
-                required: true
-            },
-            MERGE0: {
-                required: true,
-                email: true
-            }
+$('#signup').validate({
+    rules: {
+        MERGE1: {
+            required: true,
+            minlength: 2
         },
-        messages: {
-            MERGE1: {
-                required: "Por favor preencha seu nome",
-                minlength: "Seu nome deve possuir mais de 2 caracteres"
-            },
-            MERGE2: {
-                required: "Por favor preencha seu sobrenome"
-            },
-            MERGE0: {
-                required: "Por favor preencha seu endereço de email",
-                email: "Seu endereço de email deve ser válido"
-            }
+        MERGE2: {
+            required: true
         },
-        // submitHandler: function(form) {
-        //     $(form).ajaxSubmit({
-        //         type:"POST",
-        //         data: $(form).serialize(),
-        //         url:"inc/signup.php",
-        //         success: function() {
-        //             $('#signup :input').attr('disabled', 'disabled');
-        //             $('#signup').fadeTo( "slow", 0.15, function() {
-        //                 $(this).find(':input').attr('disabled', 'disabled');
-        //                 $(this).find('label').css('cursor','default');
-        //                 $('#success').fadeIn();
-        //             });
-        //         },
-        //         error: function() {
-        //             $('#signup').fadeTo( "slow", 0.15, function() {
-        //                 $('#error').fadeIn();
-        //             });
-        //         }
-        //     });
-        // }
-    });
-    	
-	// Trial signup form
-    $('#trial').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            email: {
-                required: true,
-                email: true
-            }
-        },
-        messages: {
-            name: {
-                required: "Por favor preencha com seu nome",
-                minlength: "Seu nome deve possuir mais de 2 caracteres"
-            },
-            email: {
-                required: "Por favor preencha com seu endereço de email"
-            }
-        },
-        submitHandler: function(form) {
-            $(form).ajaxSubmit({
-                type:"POST",
-                data: $(form).serialize(),
-                url:"inc/trial.php",
-                success: function() {
-                    $('#trial :input').attr('disabled', 'disabled');
-                    $('#trial').fadeTo( "slow", 0.15, function() {
-                        $(this).find(':input').attr('disabled', 'disabled');
-                        $(this).find('label').css('cursor','default');
-                        $('#success').fadeIn();
-                    });
-                },
-                error: function() {
-                    $('#trial').fadeTo( "slow", 0.15, function() {
-                        $('#error').fadeIn();
-                    });
-                }
-            });
+        MERGE0: {
+            required: true,
+            email: true
         }
-    });
-	
-	// Subscription form
-   	$('#subscribe').validate({
-        rules: {
-            subscribe_email: {
-                required: true,
-                email: true
-            } 
+    },
+    messages: {
+        MERGE1: {
+            required: "Por favor preencha seu nome",
+            minlength: "Seu nome deve possuir mais de 2 caracteres"
         },
-        messages: {
-            subscribe_email: {
-                required: "Por favor preencha com seu endereço de email"
-            } 
+        MERGE2: {
+            required: "Por favor preencha seu sobrenome"
         },
-        submitHandler: function(form) {
-            $(form).ajaxSubmit({
-                type:"POST",
-                data: $(form).serialize(),
-                url:"inc/subscribe.php",
-                success: function() {
-                    $('#subscribe :input').attr('disabled', 'disabled');
-                    $('#subscribe').fadeTo( "slow", 0.15, function() {
-                        $(this).find(':input').attr('disabled', 'disabled');
-                        $(this).find('label').css('cursor','default');
-                        $('#success').fadeIn();
-                    });
-                },
-                error: function() {
-                    $('#subscribe').fadeTo( "slow", 0.15, function() {
-                        $('#error').fadeIn();
-                    });
-                }
-            });
+        MERGE0: {
+            required: "Por favor preencha seu endereço de email",
+            email: "Seu endereço de email deve ser válido"
         }
-    });
+    },
+});
